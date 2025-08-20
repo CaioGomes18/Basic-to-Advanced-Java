bf13f1ca8e79ef4bc99936f27a54f7cda15641e9
CaioGomes18 <caiomarcelinog@gmail.com>
2025-08-20 15:25:35 -0300

    Fazendo mudanças

---
+++ src/edu/Caio/Inicial/EX6/Publicacao.java
@@ -0,0 +1,10 @@
+package edu.Caio.Inicial.EX6;
+
+public interface Publicacao {
+
+    public void abrir();
+    public void fechar();
+    public void folear(int p);
+    public void avancarPag();
+    public void voltarPag();
+}
