5fe6b0fcd17bcc9818940d1596f647b2d6cf3838
CaioGomes18 <caiomarcelinog@gmail.com>
2025-08-23 11:11:16 -0300

    doing some more work

---
+++ src/edu/Caio/Inicial/EX18/Biblioteca.java
@@ -0,0 +1,33 @@
+package edu.Caio.Inicial.EX18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+
+    List<Livro> livros = new ArrayList<>();
+
+    public Biblioteca(){
+        this.livros = null;
+    }
+
+    public void add(List<Livro> livros){
+        this.livros = livros;
+    }
+
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        if(this.livros != null) {
+            for(Livro i : this.livros) {
+                strRetorno += "Livro -> [Nome = "+ i.getNome() + "] [autor = " + i.getAutor() + "] [Preço = " + i.getPreco() + "]\n";
+            }
+        }
+
+        if(this.livros == null)
+            strRetorno += "null";
+
+        return strRetorno;
+    }
+}
