1f685e4935271c6a49a86b534dc6445c67b422dd
CaioGomes18 <caiomarcelinog@gmail.com>
2025-08-24 09:16:03 -0300

    finalizando essa parte

---
+++ src/edu/Caio/Inicial/EX21/Main.java
@@ -0,0 +1,24 @@
+package edu.Caio.Inicial.EX21;
+
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        Aviao voo1 = new Aviao("CDE345", "London", LocalTime.of(10, 30));
+        Aviao voo2 =  new Aviao("KUI765", "New York", LocalTime.of(14, 0));
+        Aviao voo3 = new Aviao("JUY456", "Paris", LocalTime.of(14, 0));
+
+        System.out.println("Status do Voo");
+
+        voo1.chceckStstus();
+        voo2.chceckStstus();
+        voo3.chceckStstus();
+
+        voo1.delay(35);
+        voo3.delay(67);
+
+        voo1.chceckStstus();
+        voo2.chceckStstus();
+        voo3.chceckStstus();
+    }
+}
